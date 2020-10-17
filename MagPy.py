382f4d07f493b1d0cf64cb2378cd561a5a50b889
Youlac <55055760+Youlac@users.noreply.github.com>
2020-10-17 15:11:58 +0200

    ISSOU
    - Création boucle principal
    - issou

--- MagPy.py
+++ MagPy.py
@@ -1,1 +1,23 @@
 # On définit les fonctions ici.
+import upemtk as utk
+
+win_scale_x = 0
+win_scale_y = 0
+
+def init_game(window_scale_x, window_scale_y):
+    """
+    Charge la fenetre et tout ce qui faut pour le jeu.
+    """
+    global win_scale_x
+    global win_scale_y
+    utk.cree_fenetre(window_scale_x, window_scale_y)
+    win_scale_x = window_scale_x
+    win_scale_y = window_scale_y
+
+def issou():
+    """
+    Hehehe
+    """
+    utk.image(win_scale_x // 2, win_scale_y // 2, "issou.gif")
+
+
--- Magic_Maze.py
+++ Magic_Maze.py
@@ -1,8 +1,29 @@
-#Programme principal
-import MagPy
+# Programme principal
+from MagPy import *
+import upemtk as utk
+import doctest
 
 def main():
-	pass
+    init_game(720, 480)
+
+    playing = True
+    while playing:
+        input = utk.attente_clic_ou_touche()
+        print(input)
+
+        if input[2] == "Touche":
+            if input[1] == "Escape":
+                #arret du programme
+                playing = False
+
+        if input[2] == "ClicGauche":
+            issou()
+
+
+    utk.ferme_fenetre()
+
 
 if __name__ == '__main__':
-	main()
+    print(help(utk.image))
+
+    main()
